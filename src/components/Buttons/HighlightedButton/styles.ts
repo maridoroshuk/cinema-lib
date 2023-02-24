2b75a6bb876e8ee04dc6e94f5a1805c8e3dd2619
maridoroshuk <m.doroshuk@bk.ru>
2023-02-24 16:00:50 +0300

    update color

--- src/components/Buttons/HighlightedButton/styles.ts
+++ src/components/Buttons/HighlightedButton/styles.ts
@@ -4,7 +4,7 @@ export const ButtonStyled = styled.button`
   font-weight: ${({ theme }) => theme.sizes.fontWeight[300]};
   font-size: ${({ theme }) => theme.sizes.fontSize[24]}px;
   line-height: ${({ theme }) => theme.sizes.lineHeight[54]}px;
-  color: ${({ theme }) => theme.colors.neutral000};
+  color: ${({ theme }) => theme.colors.neutral1000};
   font-family: ${({ theme }) => theme.fontFamily.Poppins};
   background-color: transparent;
   outline: none;
