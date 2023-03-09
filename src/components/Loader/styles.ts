a2384e7b8a92872849e5c7cb21153ef3d98f5a37
maridoroshuk <m.doroshuk@bk.ru>
2023-03-09 16:52:32 +0300

    update loader styles

--- src/components/Loader/styles.ts
+++ src/components/Loader/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LoaderWrapper = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: absolute;
+  top: 50%; right: 50%;
+  transform: translate(50%,-50%);
 `;
